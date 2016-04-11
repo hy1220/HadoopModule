1dbb8ea2b1379015fddb066ef6a9fd76bbd812dd
hy <1936693319@qq.com>
2016-04-11 23:46:51 +0800

    first commit

---
+++ src/main/java/com/hy/gdu/utils/Prints.java
@@ -0,0 +1,13 @@
+package main.java.com.hy.gdu.utils;
+
+/**
+ * Created by hy on 16-4-11.
+ */
+public class Prints {
+    public static void priln(String str){
+        System.out.println(str);
+    }
+    public static void pri(String str){
+        System.out.print(str+"   ");
+    }
+}
